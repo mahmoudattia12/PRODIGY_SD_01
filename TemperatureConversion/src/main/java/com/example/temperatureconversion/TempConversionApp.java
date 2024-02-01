0b0e4a2e0c8733fe64141ea76b52f483f3261a49
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2024-02-01 23:13:14 +0200

    update the app file

--- TemperatureConversion/src/main/java/com/example/temperatureconversion/TempConversionApp.java
+++ TemperatureConversion/src/main/java/com/example/temperatureconversion/TempConversionApp.java
@@ -11,8 +11,8 @@ public class TempConversionApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TempConversionApp.class.getResource("TempConversion-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 650, 400);
+        stage.setTitle("Temperature Conversion");
         stage.setScene(scene);
         stage.show();
     }
