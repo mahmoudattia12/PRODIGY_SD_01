fe2e5a7cf8311cb05eece8f531f0e16eababfd86
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2024-02-02 02:06:43 +0200

    add the calculations and their testing

--- TemperatureConversion/src/main/java/com/example/temperatureconversion/TempConversionController.java
+++ TemperatureConversion/src/main/java/com/example/temperatureconversion/TempConversionController.java
@@ -1,15 +1,14 @@
 package com.example.temperatureconversion;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class TempConversionController {
     @FXML
@@ -30,21 +29,60 @@ public class TempConversionController {
     private void onConvertButtonClick() {
         String input = inputField.getText().trim();
 
-        // Check if the input field is empty
-        if (input.isEmpty()) {
+        if(!isNumeric(input)){
             showAlert();
             return;
         }
 
         resultField.getChildren().clear();
-        resultField.getChildren().addAll(createHBox("unit1", "val1"), createHBox("unit2", "val2"));
+        double inputVal = Double.parseDouble(input);
+
+        switch (unitChoiceBox.getValue()) {
+            case "Celsius": {
+                double fahrenheit = CelsiusToFahrenheit(inputVal);
+                double kelvin = FahrenheitToKelvin(fahrenheit);
+                resultField.getChildren().addAll(createHBox("Fahrenheit: ", String.format("%.5f", fahrenheit) + " °F"),
+                                                createHBox("Kelvin: ", String.format("%.5f", kelvin) + " °K"));
+                break;
+            }
+            case "Fahrenheit": {
+                double kelvin = FahrenheitToKelvin(inputVal);
+                double celsius = KelvinToCelsius(kelvin);
+                resultField.getChildren().addAll(createHBox("Celsius: ", String.format("%.5f", celsius) + " °C"),
+                                                createHBox("Kelvin: ", String.format("%.5f", kelvin) + " °K"));
+                break;
+            }
+            default: {
+                double celsius = KelvinToCelsius(inputVal);
+                double fahrenheit = CelsiusToFahrenheit(celsius);
+                resultField.getChildren().addAll(createHBox("Celsius: ", String.format("%.5f", celsius) + " °C"),
+                                                createHBox("Fahrenheit: ", String.format("%.5f", fahrenheit) + " °F"));
+            }
+        }
+    }
+
+    public double CelsiusToFahrenheit(double celsius) {
+        return (9.0 / 5) * celsius + 32;
+    }
+
+    public double FahrenheitToKelvin(double fahrenheit) {
+        return (5.0 / 9) * (fahrenheit - 32) + 273.15;
+    }
+
+    public double KelvinToCelsius(double kelvin) {
+        return kelvin - 273.15;
     }
 
     private void showAlert() {
         Alert alert = new Alert(Alert.AlertType.WARNING);
         alert.setTitle("Input Error");
+
+        Image icon = new Image(String.valueOf(getClass().getResource("warning.png")));
+        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(icon);
+
         alert.setHeaderText(null);
-        alert.setContentText("Please enter the temperature value first!");
+        alert.setContentText("Please enter a numeric value for the temperature!");
         alert.showAndWait();
     }
 
@@ -71,13 +109,25 @@ public class TempConversionController {
         if (!isValidChar(input, event.getCharacter().charAt(0))) {
             event.consume();
             // Remove the wrong character from the text field
-            if(indexPos != -1)
-                inputField.setText(input.substring(0, indexPos) + input.substring(indexPos + 1));
+            if (indexPos != -1) inputField.setText(input.substring(0, indexPos) + input.substring(indexPos + 1));
             inputField.positionCaret(indexPos);
         }
     }
 
-    private boolean isValidChar(String input, char curr){
-        return Character.isDigit(curr) || (curr == '.' && input.chars().filter(ch -> ch == '.').count() == 1);
+    private boolean isValidChar(String input, char curr) {
+        return Character.isDigit(curr)
+                || (curr == '.' && input.chars().filter(ch -> ch == '.').count() == 1)
+                || (input.length() == 1 && curr == '-');
+    }
+
+    public boolean isNumeric(String str) {
+        try {
+            // Try to parse the string as a double
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            // If an exception is thrown, the string is not a number
+            return false;
+        }
     }
 }
---
+++ TemperatureConversion/src/test/java/com/example/temperatureconversion/TempConversionControllerTest.java
@@ -0,0 +1,40 @@
+package com.example.temperatureconversion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TempConversionControllerTest {
+    private final TempConversionController conversionController = new TempConversionController();
+    private final double delta = 0.000001;
+
+    @Test
+    void testIsNumeric() {
+        assertTrue(conversionController.isNumeric("123"));
+        assertTrue(conversionController.isNumeric("123.0320"));
+        assertFalse(conversionController.isNumeric("-"));
+        assertFalse(conversionController.isNumeric(""));
+        assertFalse(conversionController.isNumeric("123.a"));
+        assertFalse(conversionController.isNumeric("abd"));
+    }
+
+    @Test
+    void testCelsiusToFahrenheit() {
+        assertEquals(conversionController.CelsiusToFahrenheit(0), 32, delta);
+        assertEquals(conversionController.CelsiusToFahrenheit(-123), -189.4, delta);
+        assertEquals(conversionController.CelsiusToFahrenheit(456.4356), 853.58408, delta);
+    }
+    @Test
+    void testFahrenheitToKelvin() {
+        assertEquals(conversionController.FahrenheitToKelvin(0), 255.37222222, delta);
+        assertEquals(conversionController.FahrenheitToKelvin(-123), 187.03888889, delta);
+        assertEquals(conversionController.FahrenheitToKelvin(456.4356), 508.94755556, delta);
+    }
+
+    @Test
+    void testKelvinToCelsius() {
+        assertEquals(conversionController.KelvinToCelsius(0), -273.15, delta);
+        assertEquals(conversionController.KelvinToCelsius(-123), -396.15, delta);
+        assertEquals(conversionController.KelvinToCelsius(456.4356), 183.2856, delta);
+    }
+}
