0e77580b8b55e12b04eb7ee18bf67ed713b51717
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2024-02-02 03:20:41 +0200

    create the executable jar file

---
+++ TemperatureConversion/src/main/java/com/example/temperatureconversion/Launcher.java
@@ -0,0 +1,12 @@
+package com.example.temperatureconversion;
+
+import javafx.application.Application;
+
+import static javafx.application.Application.launch;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        Application.launch(TempConversionApp.class, args);
+    }
+}
